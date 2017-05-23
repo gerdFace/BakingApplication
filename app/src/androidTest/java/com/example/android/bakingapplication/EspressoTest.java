4d7bdb89ad0432a54922ce2b6b02476ab1c89987
gerdface <stevenagerdes@gmail.com>
2017-05-22 22:57:23 -0500

    Single card with recipe overview, 1 expresso test

---
+++ app/src/androidTest/java/com/example/android/bakingapplication/EspressoTest.java
@@ -0,0 +1,28 @@
+package com.example.android.bakingapplication;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class EspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void checkDessertNameOnStart() {
+        onView(withId(R.id.dessert_name)).check(matches(withText("Nutella Pie")));
+    }
+}
