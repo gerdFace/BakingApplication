dcf4978cf31ed44eaf5d6f376478f418f319ef6f
gerdface <stevenagerdes@gmail.com>
2017-06-01 12:12:40 -0500

    Added food name as actionbar title

--- app/src/main/java/com/example/android/bakingapplication/MasterRecipeDetailFragment.java
+++ app/src/main/java/com/example/android/bakingapplication/MasterRecipeDetailFragment.java
@@ -1,16 +1,13 @@
 package com.example.android.bakingapplication;
 
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -18,6 +15,8 @@ import butterknife.ButterKnife;
  * A simple {@link Fragment} subclass.
  */
 public class MasterRecipeDetailFragment extends Fragment {
+
+    public static final String TAG = RecipeDetailActivity.class.getClass().getSimpleName();
 
     @BindView(R.id.ingredients)
     TextView tVIngredients;
@@ -31,12 +30,12 @@ public class MasterRecipeDetailFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         recipe = ((RecipeDetailActivity)this.getActivity()).getRecipe();
+        Log.d(TAG, "onActivityCreated: " + recipe.getDessertName());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_master_recipe_detail, container, false);
         ButterKnife.bind(this, view);
         tVIngredients.setText(R.string.detail_fragment_ingredients_title);
--- app/src/main/java/com/example/android/bakingapplication/RecipeDetailActivity.java
+++ app/src/main/java/com/example/android/bakingapplication/RecipeDetailActivity.java
@@ -1,6 +1,5 @@
 package com.example.android.bakingapplication;
 
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -15,7 +14,7 @@ public class RecipeDetailActivity extends AppCompatActivity {
 
         recipe = getIntent().getParcelableExtra("selected_recipe");
 
-
+        setTitle(recipe.getDessertName());
     }
 
     public KRecipe getRecipe() {
