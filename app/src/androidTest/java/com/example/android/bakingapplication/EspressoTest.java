7dadaba677fcd8a48c535a4c108b8aa4fd50e8f5
gerdface <stevenagerdes@gmail.com>
2017-06-01 18:54:09 -0500

    Fixed formatting issues

--- app/src/androidTest/java/com/example/android/bakingapplication/EspressoTest.java
+++ app/src/androidTest/java/com/example/android/bakingapplication/EspressoTest.java
@@ -11,32 +11,14 @@ import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-/**
- * Instrumentation test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 @RunWith(AndroidJUnit4.class)
 public class EspressoTest {
 
-    /* REVIEW: TIGHTEN UP COMMENT--
-    *  Had to cherry-pick these tests from different branch. :-(
-    */
-
-    /* REVIEW: QUESTION--
-     * Do you even need this activity test rule?
-     */
-
-//    Isn't the activity test rule necessary for starting the activity that holds the views being tested?
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-    /* REVIEW: FIX ME--
-     *  With the changes I made, all four of these tests should all fail.
-     *  Instead, all four are reporting false positives.
-     *  What's the dealio?
-     */
 
-//
+
     @Test
     public void whenMainActivityLaunchesTextViewsAreDisplayed() {
         onView(withText("Nutella Pie")).check(matches(isDisplayed()));
--- app/src/main/java/com/example/android/bakingapplication/MainActivity.java
+++ app/src/main/java/com/example/android/bakingapplication/MainActivity.java
@@ -6,15 +6,9 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    /* REVIEW: FEATHER IN YOUR CAP COMMENT--
-     *   Unless you're actually doing something with the View/Resource/Asset/whatever that you specify by ID in the annotation,
-     *   you don't even need lines #17-30. Not hurting anything and the next story will probably utilize them.
-     */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
     }
 }
