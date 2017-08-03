4d7f929b2ff8c583228363e7fff550548e5852e8
gerdface <stevenagerdes@gmail.com>
2017-08-03 11:46:02 -0500

    Updated UI with constraintSet

--- app/src/main/java/com/example/android/bakingapplication/view/fragment/DetailListFragment.java
+++ app/src/main/java/com/example/android/bakingapplication/view/fragment/DetailListFragment.java
@@ -91,7 +91,7 @@ public class DetailListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_detail_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail_list_before, container, false);
 
         ((BakingApplication)getActivity().getApplication()).getRecipeRepositoryComponent().inject(this);
 
